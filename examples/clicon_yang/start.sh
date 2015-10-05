b8175ee8f9bf031012840ee34c69e3a1c39809ac
Olof Hagsand <olof@hagsand.se>
2015-10-05 09:27:11 +0200

    docs

--- examples/clicon_yang/start.sh
+++ examples/clicon_yang/start.sh
@@ -2,6 +2,8 @@
 # Script reads yang spec from stdin then start clicon_cli
 read -d $'\cd' -p "input yang spec(end with ^d). Or just ^d for default spec> " spec
 if [ "$spec" != "" ]; then
-    echo $spec > /usr/local/share/clicon_yang/yang/clicon_yang.yang
+    echo "module datamodel{" > /usr/local/share/clicon_yang/yang/clicon_yang.yang
+    echo $spec >> /usr/local/share/clicon_yang/yang/clicon_yang.yang
+    echo "}" >> /usr/local/share/clicon_yang/yang/clicon_yang.yang
 fi
 clicon_cli -cf /usr/local/etc/clicon_yang.conf
